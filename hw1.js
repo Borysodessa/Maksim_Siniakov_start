a655fab3d9ad552e8755a5f651dc915c59d7176b
BorysGull <chayka2021@yahoo.com>
2022-11-13 17:26:20 +0200

    task5 finish

--- hw1.js
+++ hw1.js
@@ -55,6 +55,21 @@
 // }
 
                             //--------------------task5-------------------//
+                            
+//  Создайте программу, которая фильтрует список строк и возвращает список, содержащий только имена ваших друзей.
+// Если в имени ровно 4 буквы, можете быть уверены, что оно должно быть вашим другом! В противном случае, вы можете быть уверены, что он не...
+// Пример: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]     
+
+function friend(friends) {
+    const filteredFriends = [];
+    for (const friend of friends) {
+        if (friend.length === 4) {
+            filteredFriends.push(friend);
+      }
+    }
+    return filteredFriends;
+}
+console.log(friend(["Ryan", "Kieran", "Jason", "Yous", "Olga", "Makar"]));
 
 
                          
